f0f4797b6dd12f690452887f31ebab8fb7537137
shikhar249 <43881057+shikhar249@users.noreply.github.com>
2020-08-08 12:55:46 +0800

    Add files via upload

--- all_codes/onionmhc.py
+++ all_codes/onionmhc.py
@@ -4,11 +4,12 @@ import data
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import load_model
 import os, sys
-import benchmark
+import omhc_inp
 import tensorflow as tf
 import numpy as np
-from sklearn.metrics import roc_auc_score
-from scipy.stats import spearmanr
+from sklearn.metrics import (roc_auc_score, precision_recall_curve,
+    auc, accuracy_score, f1_score)
+from scipy.stats import pearsonr, spearmanr
 import pandas as pd
 from tensorflow.keras.models import model_from_json
 
@@ -16,7 +17,6 @@ from tensorflow.keras.models import model_from_json
 parser = argparse.ArgumentParser(description='Making Predictions using OnionMHC', formatter_class=RawTextHelpFormatter)
 parser.add_argument('-struc', type=str, help="File containing structure based features in .csv format as produced by \"generate_features_cam_o.py\"")
 parser.add_argument('-seq', type=str, help="File containing the sequence of 9-mer peptides")
-# parser.add_argument('-mod', nargs="*", help="Models to evaluate (*.h5)")
 parser.add_argument('-out', help="Output file to save results to ")
 
 args = parser.parse_args()
@@ -26,22 +26,22 @@ sc = StandardScaler()
 sc.fit(df_features)
 
 
-def evaluat(seq_int, sc):
+def evaluat( seq_int, sc):
 
     seq_dict = {0: 'emb', 1: 'enc', 2: 'bls'}
-    struc = benchmark.struc_lab(args.struc)
-    seq = benchmark.seq_inf(seq_dict[seq_int], args.seq)
+    struc = omhc_inp.struc_lab(args.struc)
+    seq = omhc_inp.seq_inf(seq_dict[seq_int], args.seq)
     seq = tf.convert_to_tensor(seq, dtype=tf.float64)
     struc = sc.transform(struc)
     struc = struc.reshape(-1, 1, 60, 64)
 
     for i in range(5):
         for j in range(3):
-            json_f = open(main_dir + "models/fold" + str(i) + "_model" + str(j) + "_bls_lstm.json", 'r')
+            json_f = open("/home/shikhar/peptide_proj/model_files/models/cross_val/fold" + str(i) + "_model" + str(j) + "_bls_lstm.json", 'r')
             loaded_model_json = json_f.read()
             json_f.close()
             model = model_from_json(loaded_model_json)
-            model.load_weights((main_dir + "models/fold" + str(i) + "_model" + str(j) + "_bls_lstm_weights.h5"))
+            model.load_weights(("/home/shikhar/peptide_proj/model_files/models/cross_val/fold" + str(i) + "_model" + str(j) + "_bls_lstm_weights.h5"))
             y_pred = list(np.squeeze(model.predict([struc, seq])))
             if i == 0 and j == 0:
                 bench_ = y_pred
@@ -54,8 +54,6 @@ def evaluat(seq_int, sc):
 
     bench_ = [x/15 for x in bench_]
     
-    print(bench_)
-    
     return bench_
 
 
@@ -67,4 +65,9 @@ d = {'peptide_sequences': sequences, 'OnionMHC_score': output, 'Binding_Affinity
 
 output_df = pd.DataFrame(data=d)
 
-output_df.to_csv(args.out, sep='\t', index=False, float_format="%.4f")
+
+if args.out:
+    output_df.to_csv(args.out, sep='\t', index=False, float_format="%.4f")
+else:
+    print(output_df)
+
